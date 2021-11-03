ed32bb98ab09cf18ed34bfc95dc875bdb50d9eca
sachin maharjan <sachin.maharjan@dishhome.com.np>
2021-11-03 15:23:43 +0545

    make table for extension number

---
+++ dist/main.js
@@ -0,0 +1,354 @@
+const EXT_DETAIL = [
+    {
+        id : 1,
+        name: 'Aditya Bhattrai',
+        designation: 'Manager-STB & QA',
+        extNo: '416'
+    },
+    {
+        id : 2,
+        name: 'Amit Shakya',
+        designation: 'Manager-Broadcast Operations and Engineering',
+        extNo: '405'
+    },
+    {
+        id : 3,
+        name: 'Anil Thakur',
+        designation: 'Merchandising Assistant',
+        extNo: '321'
+    },
+    
+    {
+        id : 4,
+        name: 'Ashok Basnet',
+        designation: 'Manager-Broadcast Operations and Engineering',
+        extNo: '405'
+    },
+    
+    {
+        id : 5,
+        name: 'Audit Cabin',
+        designation: '',
+        extNo: '124'
+    },
+    
+    {
+        id : 6,
+        name: 'Automation(safal, Bhuvan, Rajiv)',
+        designation: 'Automation',
+        extNo: '407'
+    },
+    
+    {
+        id : 7,
+        name: 'Banita Munaankarmi',
+        designation: 'HT and Admin Assistant',
+        extNo: '308'
+    },
+    
+    {
+        id : 8,
+        name: 'Bhupal Singh Basnet',
+        designation: 'Business Developement Manager',
+        extNo: '310'
+    },
+    ,
+    {
+        id : 9,
+        name: 'Bikash Dahal/ Bipina KC',
+        designation: 'Associate HR/HR Assistant',
+        extNo: '316'
+    },
+    
+    {
+        id : 10,
+        name: 'Bimee Maskey/Bimosh Gopali',
+        designation: 'Brand Officer/ Associate Brand',
+        extNo: '312'
+    },
+    
+    {
+        id : 11,
+        name: 'Binod Majju Shrestha',
+        designation: 'Sr.Logistics and Procurement Officer',
+        extNo: '105'
+    },
+    
+    {
+        id : 12,
+        name: 'Binod Subedi/Ram K Shilakar',
+        designation: 'Associate Sales/Sales Officer',
+        extNo: '212'
+    },
+    
+    {
+        id : 13,
+        name: 'Board Room',
+        designation: '',
+        extNo: '303'
+    },
+    
+    {
+        id : 14,
+        name: 'Chandra k Khatrai',
+        designation: 'Associate Finance Officer',
+        extNo: '121'
+    },
+    
+    {
+        id : 15,
+        name: 'Dealer Support',
+        designation: '',
+        extNo: '205'
+    },
+    
+    {
+        id : 16,
+        name: 'Deepa Thapa/Sajana Aryal',
+        designation: 'Manager-Broadcast Operations and Engineering',
+        extNo: '100/101'
+    },
+    
+    {
+        id : 17,
+        name: 'Dinesh Ratna',
+        designation: 'Associate Brand Officer',
+        extNo: '319'
+    },
+    
+    {
+        id : 18,
+        name: 'Dinesh Tako',
+        designation: 'Accounts Officer',
+        extNo: '110'
+    },
+    
+    {
+        id : 19,
+        name: 'Dipak Poudel',
+        designation: 'Accounts Assistant',
+        extNo: '108'
+    },
+    
+    {
+        id : 20,
+        name: 'Electrical Department and Earth Station',
+        designation: '',
+        extNo: '409/414'
+    },
+    
+    {
+        id : 21,
+        name: 'Electrical Room Corporate',
+        designation: '',
+        extNo: '218'
+    },
+    
+    {
+        id : 22,
+        name: 'Elina Thapa',
+        designation: 'Associate Sales Officer',
+        extNo: '216'
+    },
+    
+    {
+        id : 23,
+        name: 'Geeta Pandey',
+        designation: 'HR and  Admin Supervisor',
+        extNo: '315'
+    },
+
+    {
+        id: 24,
+        name: 'Gopal Prasad Nyaupane',
+        designation: 'Software Developer',
+        extNo: '422'
+    },
+
+    {
+        id: 25,
+        name: 'Guard Room',
+        designation: ' ',
+        extNo: '401'
+    },
+    {
+        id: 26,
+        name: 'Hardware Technician',
+        designation: 'Hardware Technician',
+        extNo: '120'
+    },
+    {
+        id: 27,
+        name: 'Hikmat Saud/Ranjan Khakurel',
+        designation: 'Database Administrator/ Software Developer',
+        extNo: '421'
+    },
+    {
+        id: 28,
+        name: 'IT support',
+        designation: 'Rupak, Niraj, Dipak, Tsewang, Shubham and Aman ',
+        extNo: '206'
+    },
+    {
+        id: 29,
+        name: 'Jas Bhadur Gurung',
+        designation: ' Reginoal sales Manager',
+        extNo: '210'
+    },
+    {
+        id: 30,
+        name: 'Jyotsna Bajracharya',
+        designation: 'Sr. HR Officer',
+        extNo: '306'
+    },
+
+    {
+        id: 31,
+        name: 'Khem R Pokhrel/Sushan KC',
+        designation: 'Data Analyst/Associate DBA ',
+        extNo: '424'
+    },
+    {
+        id: 32,
+        name: 'Kitchen-Corporate',
+        designation: ' ',
+        extNo: '217'
+    },
+    {
+        id: 33,
+        name: 'Kitchen-EarthStation',
+        designation: ' ',
+        extNo: '403'
+    },
+    {
+        id: 34,
+        name: 'Krishna Thapa',
+        designation: 'Account Assistant ',
+        extNo: '111'
+    },
+    {
+        id: 35,
+        name: 'Kushal Phuyal',
+        designation: 'Associate Logistic and Procrument',
+        extNo: '128'
+    },
+    {
+        id: 36,
+        name: 'Mahendra Bahadur KC',
+        designation: 'Sr.Finance Manager ',
+        extNo: '107'
+    },
+    {
+        id: 37,
+        name: 'Mandu Bajracharya',
+        designation: 'Sr. HR Officer',
+        extNo: '305'
+    },
+
+    {
+        id: 38,
+        name: 'Manish/Suman',
+        designation: 'Data Analyst/Associate DBA ',
+        extNo: '118'
+    },
+    {
+        id: 39,
+        name: 'Manoj KC',
+        designation: ' GM_IT &SMS',
+        extNo: '410'
+    },
+
+    {
+        id: 40,
+        name: 'Manoj Sharma',
+        designation: 'Regional Sales Manager',
+        extNo: '209'
+    },
+    {
+        id: 41,
+        name: 'MCR',
+        designation: ' ',
+        extNo: '444'
+    },
+    {
+        id: 42,
+        name: 'Meeting Room - Ground Floor',
+        designation: ' ',
+        extNo: '125'
+    },
+    {
+        id: 43,
+        name: 'Mukesh Kumar Shah',
+        designation: 'Sr. Finance Officer ',
+        extNo: '113'
+    },
+    {
+        id: 44,
+        name: 'Neha Joshi',
+        designation: 'HR Manager ',
+        extNo: '304'
+    },
+
+    {
+        id: 45,
+        name: 'Nirmal Acharya',
+        designation: 'Senior Sales Officer ',
+        extNo: '214'
+    },
+    {
+        id: 46,
+        name: 'Niru Shrestha',
+        designation: 'Associate Accounts Officer',
+        extNo: '122'
+    },
+    {
+        id: 47,
+        name: 'Nitesh Sharma Ghimire',
+        designation: 'Manager-Enterprise Applications ',
+        extNo: '412'
+    },
+    {
+        id: 48,
+        name: 'Omishan Thapa',
+        designation: 'Motion Graphics Designer',
+        extNo: '318'
+    },
+
+];
+
+
+let tbody_content = '<tr>';
+EXT_DETAIL.forEach( (item) => {
+    tbody_content += '<td>';
+    tbody_content += item['id'];
+    tbody_content += "</td><td>";
+    tbody_content += item["name"];
+    tbody_content += "</td><td>";
+    tbody_content += item["designation"];
+    tbody_content += "</td><td>";
+    tbody_content += item["extNo"];
+    tbody_content += "</td></tr>";
+});
+const tbody = document.querySelector("#table-body");
+tbody.innerHTML = tbody_content;
+
+
+const inputbox = document.querySelector('#search_text');
+inputbox.addEventListener('keyup', (e) =>{
+    e.preventDefault();
+    let search_text = inputbox.value; 
+    let result  = EXT_DETAIL.filter(item => item.name.includes(search_text) || item.designation.includes(search_text))
+    tbody_content = '<tr>';
+    result.forEach((item)=>{
+        tbody_content += '<td>';
+        tbody_content += item['id'];
+        tbody_content += "</td><td>";
+        tbody_content += item["name"];
+        tbody_content += "</td><td>";
+        tbody_content += item["designation"];
+        tbody_content += "</td><td>";
+        tbody_content += item["extNo"];
+        tbody_content += "</td></tr>";
+    });
+    tbody.innerHTML = tbody_content;
+})
